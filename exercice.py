7606ca7a3f017d97474c7e043a9733e73fb2bf34
JiminKH <linakhial@gmail.com>
2020-09-09 17:53:32 -0400

    LK : Completed functions

--- exercice.py
+++ exercice.py
@@ -5,11 +5,13 @@ import math
 
 def square_root(number: int) -> float:
     # TODO completer la fonction
+
     try : 
         math.sqrt(number)
     except ValueError :
         print("La valeur est negative!")
         return
+        
     return math.sqrt(number)
 
 
